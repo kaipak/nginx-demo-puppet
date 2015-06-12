57cece2a5169e1837ac0afd414432100c477eb22
Kai Pak <kai@kaipak.org>
2015-06-12 15:54:07 -0700

    Use jfryman module for nginx instead.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,41 +1,28 @@
 # Configure nginx webpage and serve up demo page on port 8000
+# Require: maestrodev/wget, jfryman/nginx
 #
-
 class nginx_puppet_demo (
   $nginx_conf = '/etc/nginx/sites-available/puppet-nginx.conf',
-  $index_loc  = '/usr/share/nginx/html/puppet/'
+  $www_root  = '/usr/share/nginx/html/puppet/'
 ) {
-
-  package { 'nginx':
-    ensure  => 'installed',
+  
+  class { 'nginx': }
+  
+  nginx::resource::vhost{ 'localhost':
+    listen_port  => 8000,
+    www_root     => $www_root,
   }
 
-  file { $nginx_conf:
-    ensure => 'present',
-    source => 'puppet:///modules/nginx_puppet_demo/puppet-nginx.conf',
-    require => Package['nginx'],
-  }
-
-  file { '/etc/nginx/sites-enabled/default':
-    ensure => 'link',
-    target => $nginx_conf,
-  }
-
-  file { $index_loc:
+  file { "$www_root":
     ensure    => 'directory',
+    require   => Package['nginx'],
   }
 
   wget::fetch { "Puppet Index":
     source      => 'https://raw.githubusercontent.com/puppetlabs/exercise-webpage/master/index.html',
-    destination => "${index_loc}/index.html",
+    destination => "${www_root}/index.html",
     timeout     => 0,
     verbose     => true,
-  }
-
-  service { 'nginx':
-    ensure => 'running',
-    enable => 'true',
-    require => Package['nginx'],
-    subscribe => File[$nginx_conf],
+    require     => File[$www_root],
   }
 }
